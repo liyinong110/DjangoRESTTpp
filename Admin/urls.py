27c2919a963291ffb3647f1d4e8d87df7b9d6249
Rock <243231551@qq.com>
2018-12-11 10:10:12 +0800

    添加AdminUser注册

---
+++ Admin/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from Admin import views
+
+urlpatterns = [
+    url(r'^users/', views.AdminUsersAPIView.as_view()),
+]
--- Admin/views.py
+++ Admin/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from Admin.models import AdminUser
+from Admin.serializers import AdminUserSerializer
+
+
+class AdminUsersAPIView(CreateAPIView):
+
+    serializer_class = AdminUserSerializer
+    queryset = AdminUser.objects.filter(is_delete=False)
